bcf27e9d1c31d25239433f2ff3ca2d80ee8aced0
Marcin Matląg <marcin.matlag@blackice.com.pl>
2017-01-25 23:03:01 +0100

    correct data permissions on startup

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ export DJANGO_SETTINGS_MODULE='settings_local'
 python /app/postorius_standalone/manage.py migrate --noinput
 python /app/postorius_standalone/manage.py collectstatic --noinput
 python /app/create_admin.py
+chown app:app -R ${DATADIR}
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
